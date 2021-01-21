18426773234de4da0731365a1e730650c0643140
Chelsea Houston <chelseahouston29@gmail.com>
2021-01-21 16:20:09 +0000

    delete empty cells - more refined code

--- main.rb
+++ main.rb
@@ -5,86 +5,100 @@
 # Gerry Moloney > Placement, Employment, Emerging Systems, Web Development
 
 require 'csv'
-table = CSV.parse(File.read("rubytta.csv"))
+
+table = []
+CSV.foreach("rubytta.csv") do |row|
+  table << row
+end
 
 # 1. Display the 2nd and 4th area of interest for each lecturer ---
-puts "2nd and 4th AOI for each Lecturer:"
-# Paul Barry
+puts "1. 2nd and 4th Area of Interest for each Lecturer:"
+# 01 Paul Barry
 puts "Name: #{table[1][0]}"
 puts "Area of interest 02: #{table[1][2]}"
 puts "Area of interest 04: #{table[1][4]}"
 puts "\n"
-# Chris Meudec
+# 02 Chris Meudec
 puts "Name: #{table[2][0]}"
 puts "Area of interest 02: #{table[2][2]}"
 puts "Area of interest 04: #{table[2][4]}"
 puts "\n"
-# Nigel Whyte
+# 03 Nigel Whyte
 puts "Name: #{table[3][0]}"
 puts "Area of interest 02: #{table[3][2]}"
 puts "Area of interest 04: #{table[3][4]}"
 puts "\n"
-# Austin Kinsella
+# 04 Austin Kinsella
 puts "Name: #{table[4][0]}"
 puts "Area of interest 02: #{table[4][2]}"
 puts "Area of interest 04: #{table[4][4]}"
 puts "\n"
-# Gerry Moloney
+# 05 Gerry Moloney
 puts "Name: #{table[5][0]}"
 puts "Area of interest 02: #{table[5][2]}"
 puts "Area of interest 04: #{table[5][4]}"
 puts "\n"
 
 # 2. Show each lecturers final area of interest ---
-puts "Lecturer's Final AOI:"
-# Paul Barry
+
+# delete blank cells --
+table[1] = table[1].reject { |item| item.nil? || item == '' }
+table[2] = table[2].reject { |item| item.nil? || item == '' }
+table[3] = table[3].reject { |item| item.nil? || item == '' }
+table[4] = table[4].reject { |item| item.nil? || item == '' }
+table[5] = table[5].reject { |item| item.nil? || item == '' }
+
+puts "2. Lecturer's Final Area of Interest:"
+# 01 Paul Barry
 puts "Name: #{table[1][0]}" 
-final01 = table[1][-1]
-if table[1][-1] == nil
-  final01 = table[1][-2]
-end
-puts "Final Area of Interest: #{final01}"
+puts "Final Area of Interest: #{table[1][-1]}"
 puts "\n"
-# Chris Meudec
+# 02 Chris Meudec
 puts "Name: #{table[2][0]}"
-final02 = table[2][-1]
-if table[2][-1] == nil
-  final02 = table[2][-2]
-end
-puts "Final Area of Interest: #{final02}"
+puts "Final Area of Interest: #{table[2][-1]}"
 puts "\n"
-# Nigel Whyte
+# 03 Nigel Whyte
 puts "Name: #{table[3][0]}"
-final03 = table[3][-1]
-if table[3][-1] == nil
-  final03 = table[3][-2]
-end
-puts "Final Area of Interest: #{final03}"
+puts "Final Area of Interest: #{table[3][-1]}"
 puts "\n"
-# Austin Kinsella
+# 04 Austin Kinsella
 puts "Name: #{table[4][0]}"
-final04 = table[4][-1]
-if table[4][-1] == nil
-  final04 = table[4][-2]
-end
-puts "Final Area of Interest: #{final04}"
+puts "Final Area of Interest: #{table[4][-1]}"
 puts "\n"
-# Gerry Moloney
+# 05 Gerry Moloney
 puts "Name: #{table[5][0]}"
-final05 = table[5][-1]
-if table[5][-1] == nil
-  final05 = table[5][-2]
-end
-puts "Final Area of Interest: #{final05}"
+puts "Final Area of Interest: #{table[5][-1]}"
 puts "\n"
 
 # 3. Display each lecturer name with the number of areas of interest they have ---
-puts "The number of AOIs each lecturer has is:"
-# Paul Barry
+puts "The number of Area of Interests each lecturer has is:"
+# 01 Paul Barry
 puts "Name: #{table[1][0]}" 
-puts table[1].count
+count01 = table[1].count
+count01 -= 1
+puts "Number of Area of Interest:s #{count01}"
 puts "\n"
-# Chris Meudec
-puts "Name: #{table[2][0]}"
-puts table[2].count
+# 02 Chris Meudec
+puts "Name: #{table[2][0]}" 
+count02 = table[2].count
+count02 -= 1
+puts "Number of Area of Interest:s #{count02}"
+puts "\n"
+# 03 Nigel Whyte
+puts "Name: #{table[3][0]}"
+count03 = table[3].count
+count03 -= 1
+puts "Number of Area of Interest:s #{count03}"
+puts "\n"
+# 04 Austin Kinsella
+puts "Name: #{table[4][0]}"
+count04 = table[4].count
+count04 -= 1
+puts "Number of Area of Interest:s #{count04}"
+puts "\n"
+# 05 Gerry Moloney
+puts "Name: #{table[5][0]}"
+count05 = table[5].count
+count05 -= 1
+puts "Number of Area of Interest:s #{count05}"
 puts "\n"
